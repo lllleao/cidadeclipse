82b159f312a0579f4c88f76d35bfdf88c8452a66
leao <lucasleaolima@gmail.com>
2024-06-19 13:04:58 -0300

    first commig

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+
+const sassCompiler = () => {
+    return gulp.src('./source/styles/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.parallel(sassCompiler))
+}
