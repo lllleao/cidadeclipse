81b133db33a73938bc89ce874278ef0412819275
leao <lucasleaolima@gmail.com>
2024-07-19 15:31:20 -0300

    carrossel finalmente acabou

--- source/scripts/carrousel.js
+++ source/scripts/carrousel.js
@@ -1,35 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const carrousselContainer = document.querySelector('.carroussel')
     const carrousselItems = document.querySelectorAll('.carroussel a')
     const targetRight = document.querySelector('.mask-right')
     const targetLeft = document.querySelector('.mask-left')
-    let state = {
-        transition: 1,
-        count: 0
+    let widthSaved = 0
+    let width = carrousselItems[0].clientWidth
+    
+    const nextSlide = () => {
+        let position = nextPrev(width)
+        carrousselItems.forEach((item) => {
+            item.style.cssText = `transform: translateX(${position}px);`
+            item.style.transition = 'transform 0.3s'
+        })
+    }
+    
+    const prevSlide = () => {
+        let position = backPrev(width)
+        carrousselItems.forEach((item) => {
+            item.style.cssText = `transform: translateX(${position}px);`
+            item.style.transition = 'transform 0.3s'
+        })
+    }
+    
+    const backPrev = (width) => {
+        const position = widthSaved + (width + 16)
+        widthSaved = position
+        if (widthSaved === ((width + 16) * 3)) {
+            widthSaved = 0
+        }
+        return position
+    }
+    
+    const nextPrev = (width) => {
+        const position = widthSaved - (width + 16)
+        widthSaved = position
+        console.log(widthSaved)
+        if (widthSaved === ((width + 16) * -3)) {
+            widthSaved = 0
+        }
+        return position
+    }
+    
+    const loop = (event) => {
+        const ver = widthSaved === 0
+        if (ver) {
+            event.style.cssText = `transform: translateX(${0});`
+            event.style.transition = 'none'
+        }
     }
     
     targetRight.addEventListener('click', () => {
-        console.log(state.transition)
-        carrousselItems.forEach(item => {
-            item.style.cssText = `transform: translateX(-${state.transition}%);`
-            item.style.transition = 'transform 0.3s'
-        })
-        
-        state.transition += 104
-        state.count++
-        console.log(state.count)
-        state.count = state.count === 4 ? 1 : state.count
-    })
-    carrousselItems.forEach(item => {
-        item.addEventListener('transitionend', () => {
-            // console.log(state.count)
-            if (state.count === 3) {
-                state.count = 3
-                item.style.cssText = `transform: translateX(109%);`
-                item.style.transition = 'none'
-                state.transition = 1
-            }
-        })
+        nextSlide()
     })
 
+    targetLeft.addEventListener('click', () => {
+        prevSlide()
+    })
+
+    carrousselItems.forEach(item => {
+        item.addEventListener('transitionend', (event) => {
+            loop(event.target)
+        })
+    })
 })
